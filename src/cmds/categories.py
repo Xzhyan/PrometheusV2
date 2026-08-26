3c2476f828ad202bbf13e84e1feb2a94f2797c02
malcondev.lds@hotmail.com <malcondev.lds@hotmail.com>
2026-08-26 15:51:41 -0300

    estruturando

---
+++ src/cmds/categories.py
@@ -0,0 +1,17 @@
+
+
+
+def list_commands(list, name):
+    """Lista os comandos e separa por categoria"""
+
+
+
+
+CATEGORY_COMMANDS = {
+    'clear': {
+        'desc': "",
+        'usage': "",
+        'handler': ""
+    },
+}
+
--- src/ui/banners.py
+++ src/ui/banners.py
@@ -7,11 +7,8 @@ class Banners:
     """Banners em ASCII da ferramenta"""
 
     TOOL_LOGO = f"""{Colors.FG_ONE}
-      ██████╗ ██████╗  ██████╗ ███╗   ███╗███████╗████████╗██╗  ██╗███████╗██╗   ██╗███████╗
-      ██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔════╝╚══██╔══╝██║  ██║██╔════╝██║   ██║██╔════╝
-      ██████╔╝██████╔╝██║   ██║██╔████╔██║█████╗     ██║   ███████║█████╗  ██║   ██║███████╗
-      ██╔═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██╔══╝     ██║   ██╔══██║██╔══╝  ██║   ██║╚════██║
-      ██║     ██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗   ██║   ██║  ██║███████╗╚██████╔╝███████║
-      ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
-      {Colors.FG_ONE}Developed by {Colors.FG_TWO}{settings.AUTHOR} {Colors.FG_ONE}- version: {Colors.FG_TWO}{settings.VERSION} {Colors.FG_ONE}- team: {Colors.FG_TWO}{settings.TEAM}"""
+                    ┏┓  ┳┓  ┏┓  ┳┳┓  ┏┓  ┏┳┓  ┓┏  ┏┓  ┳┳  ┏┓
+                    ┃┃  ┣┫  ┃┃  ┃┃┃  ┣    ┃   ┣┫  ┣   ┃┃  ┗┓
+                    ┣┛  ┛┗  ┗┛  ┛ ┗  ┗┛   ┻   ┛┗  ┗┛  ┗┛  ┗┛
+            {Colors.FG_ONE}<< Developed by {Colors.FG_TWO}{settings.AUTHOR} {Colors.FG_ONE}- ver: {Colors.FG_TWO}{settings.VERSION} {Colors.FG_ONE}- {Colors.FG_TWO}{settings.TEAM} {Colors.FG_ONE}>>"""
 
